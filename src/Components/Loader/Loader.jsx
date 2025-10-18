4160dbebea34f5737da5dd7141f91670776e688b
mashudasiddikamaisha <mashudamaisha@gmail.com>
2025-10-18 13:34:32 +0600

    updated loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div className='flex justify-center items-center w-full py-10'>
-            <span className="loading loading-spinner loading-xl"></span>
+        <div className='flex justify-center items-center w-full py-10 bg-gray-100'>
+            <div className='flex flex-col items-center gap-4'>
+                <span className="loading loading-spinner loading-xl"></span>
+                <p className='text-2xl font-bold text-gray-500'>Loading. Please Wait....</p>
+
+            </div>
         </div>
     );
 };
